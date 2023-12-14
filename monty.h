f0853489462145c68959219ff8797567a4904cd1
BildadM <bildadmuchemi.w@gmail.com>
2023-12-14 10:29:02 -0800

    header file

--- monty.h
+++ monty.h
@@ -1,3 +1,5 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -26,3 +28,5 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+#endif 
