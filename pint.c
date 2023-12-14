40b074351a2e33692cdd54ffbb7292486027ed41
wanjikumutuku <moreenkinyua4@gmail.com>
2023-12-14 12:31:01 -0800

    push and pall, bytecodes, pint and swap

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include <monty.h>
+
+/**
+ * pint - prints the value at the top of the stack.
+ * @stack: staxk values
+ * @line_number: number of lines
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "Error: can't pint an empty stack
+				at line %u\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
